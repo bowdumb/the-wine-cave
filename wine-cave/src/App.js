5fd4ce58e2559ad60ceb9cbc7ad35bedfc5383e8
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-07-26 13:57:45 -0400

    hacked together a lite version of the App.js file

--- wine-cave/src/App.js
+++ wine-cave/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
+  const title = 'The Wine Cave';
+  useEffect(() => {
+    document.title = title;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
