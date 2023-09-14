4adff6db418282f36d221b27636ee6ede56f803a
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-09-14 00:34:47 -0400

    Restructured App.js file in order to resolve useRoutes issue.

--- wine-cave/src/App.js
+++ wine-cave/src/App.js
@@ -14,16 +14,18 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Navbar />
+    <Router>
+      <div>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
