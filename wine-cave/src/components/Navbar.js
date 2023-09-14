ef5939f5d044b186bcb3b02585fe907aa9214708
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-09-14 13:01:17 -0400

    Barebones navbar links are implemented. Currently just HTML.

--- wine-cave/src/components/Navbar.js
+++ wine-cave/src/components/Navbar.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import'../styles/Navbar.css';
 
 
 function Navbar() {
-    
+    const [activeLink, setActiveLink] = useState('');
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
     return (
         <div>
             <section className = "navbar">
@@ -10,6 +17,27 @@ function Navbar() {
                     <h1 className = "navbar-title">
                         The Wine Cave
                     </h1>
+                    <nav>
+                        <div className = "navbar-links">
+                            <NavLink
+                            exact
+                            to="/"
+                            className = "nav-link"
+                            activeClassName= "active-link"
+                            onClick = {() => handleLinkClick("/")}
+                            >
+                                Home
+                            </NavLink>
+                            <NavLink
+                            to="/about"
+                            classname = "nav-link"
+                            activeClassName = "active-link"
+                            onClick = {() => handleLinkClick("/about")}
+                            >
+                                About
+                            </NavLink>
+                        </div>
+                    </nav>
                 </div>
             </section>
         </div>
